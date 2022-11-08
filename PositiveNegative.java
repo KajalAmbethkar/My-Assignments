c1cc7dc978f51d224579d4d1fb81bd3686632ec2
KajalAmbethkar <117593163+KajalAmbethkar@users.noreply.github.com>
2022-11-08 15:46:03 +0530

    PositiveNegative

---
+++ PositiveNegative.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		int num=-1;
+		if(num>0)
+		{
+			System.out.println("The number is Positive Number");
+		}
+		else
+		{
+			System.out.println("The number is Negative Number");
+		}
+
+	}
+
+}
